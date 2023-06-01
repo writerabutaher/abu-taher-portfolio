de2cdebba569b772130ed6b77da375b9b901ec8b
Abu Taher <writerabutaher@gmail.com>
2023-06-02 00:42:15 +0600

    project info

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -25,7 +25,7 @@ const Projects = () => {
                                     <div className='flex gap-4'>
                                         <a
                                             className="group relative inline-flex items-center overflow-hidden rounded-full border border-current px-8 py-3 text-white focus:outline-none focus:ring active:text-pink-500 bg-pink-600 hover:bg-white hover:text-pink-600 border-none"
-                                            href=""
+                                            href={live}
                                             target='_blank'
                                         >
                                             <span
@@ -51,7 +51,7 @@ const Projects = () => {
                                                 Live Demo
                                             </span>
                                         </a>
-                                        <a href='' target='_blank' className='bg-pink-600 text-white rounded-full p-2 border-none border-pink-600 hover:text-pink-600 hover:bg-white'>
+                                        <a href={github} target='_blank' className='bg-pink-600 text-white rounded-full p-2 border-none border-pink-600 hover:text-pink-600 hover:bg-white'>
                                             <AiFillGithub size={'2rem'} />
                                         </a>
                                     </div>
@@ -70,42 +70,39 @@ const Projects = () => {
 
 const projects = [
     {
-        name: "Project Name",
-        live: "",
-        github: "",
+        name: "Schedu Plannr",
+        live: "https://schedu-plannr.web.app/",
+        github: "https://github.com/asifsikder23/ScheduPlannr-client",
         img: "https://img.freepik.com/free-vector/back-school-landing-page_23-2148624215.jpg?size=626&ext=jpg",
         list: [
-            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>,
-            <li>Odit omnis, sunt atque ut consequuntur est. Perspiciatis.</li>,
-            <li>Vero atque mollitia ex provident hic nobis ipsum.</li>,
-            <li>Excepturi reiciendis animi magni debitis in delectus voluptatum.</li>,
-            <li>Saepe nemo unde, ut commodi omnis quam nostrum?</li>
+            <li>This provides make scheduling easier for any kinds of meetings.</li>,
+            <li>Users can Create, Update, Delete, Share, and Copy meeting links in the clipboard.</li>,
+            <li>Blog writing, Note taking etc features.Upgrade your plan to use more premium features.</li>,
+            <li>A dashboard section to manage schedule, team, availability, profile, billing, and admin panel.</li>
         ]
     },
     {
-        name: "Project Name",
-        live: "",
-        github: "",
+        name: "Orbit Zone",
+        live: "https://orbit-zone.web.app/",
+        github: "https://github.com/writerabutaher/Orbit-Zone-Client",
         img: "https://img.freepik.com/free-psd/modern-landing-page-mockup_23-2148095008.jpg?w=900&t=st=1674925898~exp=1674926498~hmac=6486c9f8f057aab6680b10786264190f50b2661127ed7158503fff7c56022d23",
         list: [
-            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>,
-            <li>Odit omnis, sunt atque ut consequuntur est. Perspiciatis.</li>,
-            <li>Vero atque mollitia ex provident hic nobis ipsum.</li>,
-            <li>Excepturi reiciendis animi magni debitis in delectus voluptatum.</li>,
-            <li>Saepe nemo unde, ut commodi omnis quam nostrum?</li>
+            <li>This is a user friendly buy and selling application.</li>,
+            <li>There are three types of categories and vehicles of any brand and model available.</li>,
+            <li>Users can post, buy and advertise cars if the user wants.</li>,
+            <li>Also, there are a dashboard section and an admin panel.</li>
         ]
     },
     {
-        name: "Project Name",
-        live: "",
-        github: "",
+        name: "Eye Care",
+        live: "https://eye-care-assignment.web.app/",
+        github: "https://github.com/writerabutaher/Eye-Care-Client",
         img: "https://img.freepik.com/free-vector/back-school-landing-page_23-2148624215.jpg?size=626&ext=jpg",
         list: [
-            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>,
-            <li>Odit omnis, sunt atque ut consequuntur est. Perspiciatis.</li>,
-            <li>Vero atque mollitia ex provident hic nobis ipsum.</li>,
-            <li>Excepturi reiciendis animi magni debitis in delectus voluptatum.</li>,
-            <li>Saepe nemo unde, ut commodi omnis quam nostrum?</li>
+            <li>A eye caring service provide this application.</li>,
+            <li>Each eye service has its own details, some users review, and a review section to give the review.</li>,
+            <li>Users can add, remove and update their own reviews.</li>,
+            <li>Also, you can add new services to this website.</li>
         ]
     },
 ]
